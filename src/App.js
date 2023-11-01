26972d30380f396e401aad38e4d1cc0ffdead6f2
weissenborn24seb@gmail.com <weissenborn24seb@gmail.com>
2023-11-01 12:54:58 +0100

    staring to build a helper workbook for supporting projects in the future

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.css';
+import PackageList from './components/PackageList/PackageList';
+
+function App() {
+  return (
+    <div className="App">
+
+      <PackageList/>
+
+    </div>
+  );
+}
+
+export default App;
